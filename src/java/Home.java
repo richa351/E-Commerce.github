71fbfcb0c74eb1677353bad01b2f5eecab8503c2
ashwani-1234 <ashwaniprakashmaurya@gmail.com>
2021-05-22 12:05:37 +0530

    third commit

--- src/java/Home.java
+++ src/java/Home.java
@@ -27,11 +27,90 @@ public class Home extends HttpServlet {
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet Home</title>");            
+            out.println("<title>Servlet Home</title>"
+                    + "<link rel=\"stylesheet\" type=\"text/css\" href=\"css/home.css\">");            
             out.println("</head>");
             out.println("<body>");
             out.println("<h1>Servlet Home at " + request.getContextPath() + "</h1>"
-                    + "<h2>hiii<h2>");
+                    + "<header >\n" +
+"        <!--Logo for website -->\n" +
+"        <div class=\"logo\">\n" +
+"            <img src=\"images/logo a.jpg\" alt=\"\">\n" +
+"        </div>\n" +
+"        <!--menu bar-->\n" +
+"        <div class=\"topnav \">\n" +
+"            <a  href=\"#home\">Notification</a>\n" +
+"            \n" +
+"            <div class=\"topnav-right\">\n" +
+"                <a href=\"#news\">Cart</a>\n" +
+"                <a class=\"active\" href=\"#contact\">Log in</a>\n" +
+"            </div>\n" +
+"          </div>\n" +
+"        \n" +
+"        <button class=\"nav-toggle\" aria-label=\"toggle navigation\">\n" +
+"            <span class=\"hamburger\"></span>\n" +
+"        </button>\n" +
+"        \n" +
+"        <nav class=\"nav\">\n" +
+"            <ul class=\"nav-list\">\n" +
+"                 <li class=\"nav-item\">\n" +
+"                   <a href=\"#\" class=\"nav-link\">All Categories</a>  \n" +
+"                 </li>\n" +
+"                 <li class=\"nav-item\">\n" +
+"                    <a href=\"#services\" class=\"nav-link\">My Orders</a>  \n" +
+"                  </li>\n" +
+"                  <li class=\"nav-item\">\n" +
+"                    <a href=\"#about\" class=\"nav-link\">My Cart</a>  \n" +
+"                  </li>\n" +
+"                  <li class=\"nav-item\">\n" +
+"                    <a href=\"#work\" class=\"nav-link\">My Account</a>  \n" +
+"                  </li>\n" +
+"                  <li class=\"nav-item\">\n" +
+"                    <a href=\"#work\" class=\"nav-link\">Notification</a>  \n" +
+"                  </li>\n" +
+"                  <li class=\"nav-item\">\n" +
+"                    <a href=\"#work\" class=\"nav-link\">Help?</a>  \n" +
+"                  </li>\n" +
+"            </ul>\n" +
+"        </nav>\n" +
+"    </header>\n" +
+"\n" +
+"    <div class=\"second-menu-bar\">\n" +
+"        <a class=\"active\" href=\"#home\">All Categories</a>\n" +
+"        <a href=\"#news\">Fashion</a>\n" +
+"        <a href=\"#contact\">Electronics</a>\n" +
+"        <a href=\"#news\">Home Appliances</a>\n" +
+"        <a href=\"#contact\">Mobiels</a>\n" +
+"    </div>\n" +
+"\n" +
+"    <!--poster slider-->\n" +
+"   \n" +
+"    \n" +
+"    <section class=\"poster-slider\" >\n" +
+"        \n" +
+"      <a class=\"poster-slider-img\" href=\"\">\n" +
+"          <img src=\"images/images.png\" alt=\"poster\">\n" +
+"      </a>\n" +
+"    </section>\n" +
+"\n" +
+"\n" +
+"   \n" +
+"<script >\n"
+                    + "const navToggle= document.querySelector('.nav-toggle');\n" +
+"const navLinks = document.querySelectorAll('.nav-link')\n" +
+"navToggle.addEventListener('click',()=>{\n" +
+"    document.body.classList.toggle('nav-open');\n" +
+"});\n" +
+"navLinks.forEach(link => {\n" +
+"    link.addEventListener('click',()=>{\n" +
+"        document.body.classList.remove('nav-open');\n" +
+"\n" +
+"    })\n" +
+"    \n" +
+"})" +
+"\n" +
+"\n" +
+"</script>");
             out.println("</body>");
             out.println("</html>");
         } finally {
